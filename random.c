071df2ff9bef7bd7beb683a066da2f654ce099fb
linkslice <sparctacus@gmail.com>
2012-07-23 14:38:15 -0700

    messing with random numbers

---
+++ random.c
@@ -0,0 +1,31 @@
+#define _BSD_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+void hearts(int);
+
+int main()
+{
+
+	int count = 0;
+	while (count != 1) {
+		printf("%d\n", makeRandom());
+	}
+	
+}
+
+int makeRandom()
+{
+
+	int p;
+	p = getpid();
+	srand(time(NULL)+p);
+	int y;
+	y = 60 + (int) (1000.0 * (rand() / (RAND_MAX + 1.0)));
+
+	return y;
+}
